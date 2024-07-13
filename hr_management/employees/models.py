a37129fa422b175d9c6184a032b0cecb4d3048b1
Oleksandr Khytrov <o.khytov@gmail.com>
2024-07-13 14:10:13 +0300

    Add Employee model

--- hr_management/employees/models.py
+++ hr_management/employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    position = models.CharField(max_length=128)
+    date_of_hire = models.DateField()
+    email = models.EmailField(unique=True)
+    manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name='subordinates')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
