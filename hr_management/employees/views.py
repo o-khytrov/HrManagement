dc306fc9bf39436e49f61d18a09fcf97d5380d3e
Oleksandr Khytrov <o.khytov@gmail.com>
2024-07-13 15:52:00 +0300

    Add basic view

--- hr_management/employees/views.py
+++ hr_management/employees/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Employee
 
 
 def index(request):
-    return HttpResponse("Employees list.")
+    employees = Employee.objects.all()
+    hierarchy = build_hierarchy(employees)
+    return render(request, 'employee_hierarchy.html', {'hierarchy': hierarchy})
+
+
+def build_hierarchy(employees):
+    employee_dict = {employee.id: employee for employee in employees}
+    hierarchy = []
+
+    for employee in employees:
+        if employee.manager_id is None:
+            hierarchy.append(employee)
+        else:
+            manager = employee_dict[employee.manager_id]
+            if not hasattr(manager, 'subordinate_list'):
+                manager.subordinate_list = []
+            manager.subordinate_list.append(employee)
+
+    return hierarchy
